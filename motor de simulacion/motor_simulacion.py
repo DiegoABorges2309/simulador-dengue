1e8afb29c33603caf5f490cc9c4097f035becaa4
Diego~ <udiegoborgesdev@gmail.com>
2026-06-02 11:42:48 -0400

    listo el modelo para el push

--- motor de simulacion/motor_simulacion.py
+++ motor de simulacion/motor_simulacion.py
@@ -1,3 +1,8 @@
+"""
+Clase que se encarga de cargar e iniciar
+la simulacion
+"""
+
 from seir_sei.seir import Seir, DatosSimulacionHumanos
 from seir_sei.sei import Sei, DatosSimulacionVector
 from seir_sei.euler import Euler
@@ -13,6 +18,30 @@ class Motor:
         infectados_vectores: int,
         datos_simulacion_vector: DatosSimulacionVector,
     ):
+        """
+        Clase 'Motor' que se encarga de iniciar
+        los datos de la simulacion.
+
+        Parameters
+        ----------
+        humanos_poblacion
+            Cantidad de humanos al iniciar.
+        infectados_humanos
+            Cantidad inicial de infectados humanos
+            al inicio.
+        datos_simualcion_humanos
+            Datos necesarios para la simulacion
+            por el lado de humanos.
+        vector_poblacion
+            Cantidad inicial de vectores al inicio.
+        infectados_vectores
+            Cantidad inicial de infectados vectores
+            al inicio.
+        datos_simulacion_vector
+            Datos necesarios para la simulacion
+            por el lado de vectores.
+        """
+        self.lista_de_dias = []
         self.datos_h = datos_simualcion_humanos
         self.datos_v = datos_simulacion_vector
         self.modelo_seir = Seir(
@@ -21,15 +50,38 @@ class Motor:
         self.modelo_sei = Sei(
             vector_poblacion, infectados_vectores, datos_simulacion_vector
         )
-        self.euler = Euler(0.1, 1.0)
+        self.euler = Euler(
+            0.041, 1.0
+        )  # Uso un paso de 0.041 para que la simulacion se calcule en 24 iteraciones, 24h
 
-        self.lista_de_dias = []
+    def variable_bombeo_agua(self, dia_actual: int, lapso_t_bombeo_agua: int):
+        """
+        Funcion que se encarga de simular
+        el sistema de bombeo de agua en Punto Fijo.
+
+        Parameters
+        ----------
+        dia_actual
+            Dia actual de la simulacion
+        lapso_t_bombeo_agua
+            Cuanto tarda en dias que llegue el
+            agua nuevamente en la ciudad.
+        """
+        dia_en_ciclo = dia_actual % lapso_t_bombeo_agua
+        self.datos_v.tasa_bombeo_agua = 1.6 - (
+            1.2 * dia_en_ciclo / (lapso_t_bombeo_agua - 1)
+        )
 
     def iniciar_simulacion(self):
+        """
+        Funcion que da inicio a la simulacion
+        con los datos ingresados.
+        """
         valor = True
         dia = 0
         while valor:
             dia += 1
+            self.variable_bombeo_agua(dia, 30)
             self.euler.calculo_euler(self.modelo_seir, self.modelo_sei)
             self.euler.dias_reales = 0.0
             valores_dia_transcurrido = {
